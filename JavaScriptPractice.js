acc66c79431c98114233e3bec386d17ab199ec5c
Sophia Dauenhauer <sophia182@stmaryspdx.net>
2016-01-08 10:29:23 -0800

    This is my first publish to GitHub

---
+++ JavaScriptPractice.js
@@ -0,0 +1,12 @@
+// javascript
+
+console.log("Please work");
+
+
+ <div id='myMap' style="position:relative; width:400px; height:400px;"></div>
+
+ 
+
+  javascript: --> inside onload() function
+
+  map = new VEMap('myMap');
